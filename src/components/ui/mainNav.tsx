c608c81caf48b9f40a99906d3c01ed04e7b993b8
Ahd-3hd <ahdhani0@gmail.com>
2024-01-22 00:42:44 +0300

    fix: show hide auth buttons on session change

--- src/components/ui/mainNav.tsx
+++ src/components/ui/mainNav.tsx
@@ -2,10 +2,17 @@ import * as React from "react";
 import Link from "next/link";
 
 import { cn } from "@/lib/utils";
-import { signOut } from "@/auth";
+import { auth, signOut } from "@/auth";
 import { Button } from "./button";
 
-export function MainNav() {
+export async function MainNav() {
+  // I fucking hate next-auth
+  const session = (await auth()) as {
+    user: {
+      email: string;
+    };
+  };
+
   const handleSignOut = async () => {
     "use server";
     await signOut({ redirectTo: "http://localhost:3002/" });
@@ -16,27 +23,33 @@ export function MainNav() {
       <Link href="/" className="flex items-center space-x-2">
         <span className="inline-block font-bold">AIONION</span>
       </Link>
-
       <nav className="flex gap-6">
-        <Link
-          href="/signin"
-          className={cn(
-            "flex items-center text-sm font-medium text-muted-foreground"
-          )}
-        >
-          Sign in
-        </Link>
-        <Link
-          href="/signup"
-          className={cn(
-            "flex items-center text-sm font-medium text-muted-foreground"
-          )}
-        >
-          Sign up
-        </Link>
-        <form action={handleSignOut}>
-          <Button className="text-background font-bold">Sign out</Button>
-        </form>
+        {!session && (
+          <>
+            <Link
+              href="/signin"
+              className={cn(
+                "flex items-center text-sm font-medium text-muted-foreground"
+              )}
+            >
+              Sign in
+            </Link>
+            <Link
+              href="/signup"
+              className={cn(
+                "flex items-center text-sm font-medium text-muted-foreground"
+              )}
+            >
+              Sign up
+            </Link>
+          </>
+        )}
+
+        {session && (
+          <form action={handleSignOut}>
+            <Button className="text-background font-bold">Sign out</Button>
+          </form>
+        )}
       </nav>
     </div>
   );
