3d35e60614a5dd4264845069751b6c1fe5dd0b75
Ahd-3hd <ahdhani0@gmail.com>
2024-02-11 21:38:31 +0300

    feat: DRY services constructor

--- src/services/workspace.service.ts
+++ src/services/workspace.service.ts
@@ -1,53 +1,6 @@
-import axios, { AxiosInstance } from "axios";
-import { cookies } from "next/headers";
+import { BaseService } from "./base.service";
 
-export class WorkspaceService {
-  protected readonly instance: AxiosInstance;
-  public constructor(url: string) {
-    this.instance = axios.create({
-      baseURL: url,
-      timeout: 30000,
-      timeoutErrorMessage: "Time out!",
-    });
-
-    this.instance.interceptors.request.use((config) => {
-      const cookieStore = cookies();
-
-      const accessToken = cookieStore.get("accessToken")?.value;
-      const refreshToken = cookieStore.get("accessToken")?.value;
-
-      if (accessToken) {
-        config.headers.Authorization = `Bearer ${accessToken}`;
-        config.headers["refreshToken"] = refreshToken;
-      }
-
-      return config;
-    });
-
-    this.instance.interceptors.response.use(
-      (response) => {
-        return response;
-      },
-      async (error) => {
-        const originalRequest = error.config;
-        if (error.response?.status === 401 && !originalRequest._retry) {
-          originalRequest._retry = true;
-          const cookieStore = cookies();
-
-          const refreshToken = cookieStore.get("accessToken")?.value;
-
-          const res = await this.instance.post("/api/v1/auth/refresh", {
-            refreshToken,
-          });
-          cookieStore.set("accessToken", res.data.accessToken);
-          cookieStore.set("refreshToken", res.data.refreshToken);
-          return this.instance(originalRequest);
-        }
-        return Promise.reject(error);
-      }
-    );
-  }
-
+export class WorkspaceService extends BaseService {
   public async getWorkspaces() {
     const res = await this.instance.get("/api/v1/workspace");
     return res.data;
